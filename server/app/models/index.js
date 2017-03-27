b5fb98979f0f07da1c12ca44921ba792ba3b378d
Chinedu Ofor <chineduofor@Andelas-MacBook-Pro.local>
2017-03-27 11:48:55 +0100

    chore(.babelrc): This is important to transpile es6 codes to es5 [Start #142306035]

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
