6f125b5db0fdf57633b209fb833811c11e36ec60
Chinedu Ofor <chineduofor@Andelas-MacBook-Pro.local>
2017-03-27 11:12:09 +0100

    chore(.squelizerc): fixed hound CI errors [Fixes #142307227]

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,26 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  development: {
+    username: 'koko',
+    password: 'owonikoko',
+    database: 'dms-dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: 'koko',
+    password: 'owonikoko',
+    database: 'dms-dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  production: {
+    username: 'koko',
+    password: 'owonikoko',
+    database: 'dms-dev',
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
   }
-}
+};
