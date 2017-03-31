c6f29a32c0b0ff8098cfd5acbafdd3d9535eae18
Chinedu Ofor <chineduofor@Andelas-MacBook-Pro-38.local>
2017-03-31 15:55:07 +0100

    chore(test): add edit test for users [Delivers #142586037]

--- server/test/model/user.spec.js
+++ server/test/model/user.spec.js
@@ -27,50 +27,50 @@ describe('User Model', () => {
   describe('Create user', () => {
     it('should create a user', (done) => {
       db.Users.create(helper.regularUser)
-      .then((user) => {
-        regularUser = user.dataValues;
-        expect(user.dataValues.firstName)
-        .to.equal(helper.regularUser.firstName);
-        expect(user.dataValues.lastName)
-        .to.equal(helper.regularUser.lastName);
-        expect(user.dataValues.username)
-        .to.equal(helper.regularUser.username);
-        expect(user.dataValues.email)
-        .to.equal(helper.regularUser.email);
-        expect(user.dataValues.password)
-        .to.not.equal(helper.regularUser.password);
-        expect(user.dataValues.rolesId)
-        .to.equal(defaultRoleId);
-        done();
-      });
+        .then((user) => {
+          regularUser = user.dataValues;
+          expect(user.dataValues.firstName)
+            .to.equal(helper.regularUser.firstName);
+          expect(user.dataValues.lastName)
+            .to.equal(helper.regularUser.lastName);
+          expect(user.dataValues.username)
+            .to.equal(helper.regularUser.username);
+          expect(user.dataValues.email)
+            .to.equal(helper.regularUser.email);
+          expect(user.dataValues.password)
+            .to.not.equal(helper.regularUser.password);
+          expect(user.dataValues.rolesId)
+            .to.equal(defaultRoleId);
+          done();
+        });
     });
 
     it('should not create a user when email is invalid', (done) => {
       db.Users.create(helper.invalidEmailUser)
-      .then()
-      .catch((error) => {
-        expect(error.errors[0].message)
-        .to.equal('Input a valid email address');
-        expect(error.errors[0].type)
-        .to.equal('Validation error');
-        expect(error.errors[0].path)
-        .to.equal('email');
-        done();
-      });
+        .then()
+        .catch((error) => {
+          expect(error.errors[0].message)
+            .to.equal('Input a valid email address');
+          expect(error.errors[0].type)
+            .to.equal('Validation error');
+          expect(error.errors[0].path)
+            .to.equal('email');
+          done();
+        });
     });
 
     it('should not create password which is not up to 8 characters', (done) => {
       db.Users.create(helper.invalidPassword)
-      .then()
-      .catch((error) => {
-        expect(error.errors[0].message)
-        .to.equal('Minimum of 8 characters is required');
-        expect(error.errors[0].type)
-        .to.equal('Validation error');
-        expect(error.errors[0].path)
-        .to.equal('validatePassword');
-        done();
-      });
+        .then()
+        .catch((error) => {
+          expect(error.errors[0].message)
+            .to.equal('Minimum of 8 characters is required');
+          expect(error.errors[0].type)
+            .to.equal('Validation error');
+          expect(error.errors[0].path)
+            .to.equal('validatePassword');
+          done();
+        });
     });
   });
 
@@ -98,13 +98,16 @@ describe('User Model', () => {
         const nullField = Object.assign({}, helper.secondUser);
         nullField[field] = null;
         db.Users.create(nullField)
-        .then()
-        .catch((error) => {
-          expect(error.errors[0].message).to.equal(`${field} cannot be null`);
-          expect(error.errors[0].type).to.equal('notNull Violation');
-          expect(error.errors[0].value).to.equal(null);
-          done();
-        });
+          .then()
+          .catch((error) => {
+            expect(error.errors[0].message)
+              .to.equal(`${field} cannot be null`);
+            expect(error.errors[0].type)
+              .to.equal('notNull Violation');
+            expect(error.errors[0].value)
+              .to.equal(null);
+            done();
+          });
       });
     });
   });
@@ -120,9 +123,9 @@ describe('User Model', () => {
             expect(error.errors[0].message)
               .to.equal('This field cannot be empty');
             expect(error.errors[0].type)
-            .to.equal('Validation error');
+              .to.equal('Validation error');
             expect(error.errors[0].path)
-            .to.equal(field);
+              .to.equal(field);
             done();
           });
       });
@@ -136,9 +139,9 @@ describe('User Model', () => {
         .then((user) => {
           decryptPassword = user.validPassword(helper.regularUser.password);
           expect(decryptPassword)
-          .to.be.equal(true);
+            .to.be.equal(true);
           expect(user.password)
-          .to.not.equal(helper.regularUser.password);
+            .to.not.equal(helper.regularUser.password);
         });
     });
   });
@@ -148,28 +151,28 @@ describe('User Model', () => {
     beforeEach((done) => {
       const updateData = { firstName: 'ofor', password: 'mypassword' };
       db.Users.findById(regularUser.id)
-      .then((user) => {
-        user.update(updateData)
-        .then((upUser) => {
-          Object.assign(UpdateUser, upUser.dataValues);
-          done();
+        .then((user) => {
+          user.update(updateData)
+            .then((upUser) => {
+              Object.assign(UpdateUser, upUser.dataValues);
+              done();
+            });
         });
-      });
     });
 
     it('should ensure that password is hashed', (done) => {
       db.Users.findById(UpdateUser.id)
-      .then((user) => {
-        expect(user.dataValues.password)
-        .is.not.equal(regularUser.password);
-        expect(user.dataValues.id)
-        .to.equal(regularUser.id);
-        expect(user.dataValues.firstName)
-        .to.not.equal(regularUser.firstname);
-        expect(user.dataValues.email)
-        .to.equal(regularUser.email);
-        done();
-      });
+        .then((user) => {
+          expect(user.dataValues.password)
+            .is.not.equal(regularUser.password);
+          expect(user.dataValues.id)
+            .to.equal(regularUser.id);
+          expect(user.dataValues.firstName)
+            .to.not.equal(regularUser.firstname);
+          expect(user.dataValues.email)
+            .to.equal(regularUser.email);
+          done();
+        });
     });
   });
 });
