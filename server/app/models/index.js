9b16ca2a694b64e1a2a92446bda33e1dfe286fe0
Chinedu Ofor <chineduofor@Andelas-MacBook-Pro-38.local>
2017-04-01 12:14:17 +0100

    chore(.travis.yml): edit travis [Fixes #142586037]

--- server/app/models/index.js
+++ server/app/models/index.js
@@ -9,7 +9,7 @@ const config = require('../../config/config')[env];
 const db = {};
 
 config.use_env_variable;
-const sequelize = new Sequelize(process.env[config.use_env_variable]);
+const sequelize = new Sequelize(config.url, config);
 
 fs
   .readdirSync(__dirname)
