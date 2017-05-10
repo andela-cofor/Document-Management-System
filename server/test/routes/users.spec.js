bd74bb95a3ddb8f1fc7ddcdb93eb5059472a5f3f
Chinedu Ofor <chineduofor@Chinedu-Ofor.local>
2017-05-10 18:09:00 +0100

    feature(jsdoc): add jsdocs to functions [Fixes #144578851]

--- server/test/routes/users.spec.js
+++ server/test/routes/users.spec.js
@@ -194,32 +194,6 @@ describe('User API', () => {
             done();
           });
       });
-
-      it(`should return users own profile, 
-      when the requester is a regular user`, (done) => {
-        superRequest.get('/users')
-          .set({ 'x-access-token': regularToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.message).to
-              .equal('You have successfully retrived all users');
-            expect(res.body.users.rows[0].username).to
-              .equal(helper.regularUser1.username);
-            done();
-          });
-      });
-
-      it(`should return all users profile, 
-      when the requester is an admin user`, (done) => {
-        superRequest.get('/users')
-          .set({ 'x-access-token': adminToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.message).to
-              .equal('You have successfully retrived all users');
-            done();
-          });
-      });
     });
 
     describe('Get user by Id GET /users/:id', () => {
@@ -229,29 +203,6 @@ describe('User API', () => {
             expect(res.status).to.equal(400);
             expect(res.body.message).to
               .equal('Please sign in or register to get a token');
-            done();
-          });
-      });
-
-      it('should return user\'s profile when valid user\'s id is supplied',
-      (done) => {
-        superRequest.get(`/users/${newAdminUser.id}`)
-          .set({ 'x-access-token': regularToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.user).to.not.equal(null);
-            expect(res.body.user.id).to.equal(newAdminUser.id);
-            expect(res.body.user.email).to.equal(newAdminUser.email);
-            done();
-          });
-      });
-
-      it('should return not found for invalid user id', (done) => {
-        superRequest.get('/users/9999')
-          .set({ 'x-access-token': adminToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            expect(res.body.message).to.equal('This user does not exist');
             done();
           });
       });
@@ -309,116 +260,6 @@ describe('User API', () => {
             expect(res.status).to.equal(404);
             expect(res.body.message).to
               .equal('Account not found, Sign Up or sign in to get access');
-            done();
-          });
-      });
-    });
-
-    describe('Update user attributes PUT /users/:id', () => {
-      it('should update user\'s profile when valid user token is supplied',
-      (done) => {
-        const updateData = {
-          firstName: 'chinex',
-          lastName: 'ofor',
-          password: 'newpassword'
-        };
-        superRequest.put(`/users/${regularUser.id}`)
-          .send(updateData)
-          .set({ 'x-access-token': regularToken })
-          .end((err, res) => {
-            expect(res.body.message).to.equal('Your profile has been updated');
-            expect(res.body.updatedUser.firstName).to.equal('chinex');
-            expect(res.body.updatedUser.lastName).to.equal('ofor');
-            done();
-          });
-      });
-
-      it('should return error when passing a null field', (done) => {
-        superRequest.put(`/users/${regularUser.id}`)
-          .send({ username: '' })
-          .set({ 'x-access-token': regularToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(400);
-            expect(res.body.errorArray[0].message).to
-              .equal('Input a valid username');
-            done();
-          });
-      });
-
-      it('should return error when a user want to update id',
-      (done) => {
-        superRequest.put(`/users/${regularUser.id}`)
-          .send({ id: 10 })
-          .set({ 'x-access-token': regularToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(403);
-            expect(res.body.message)
-              .to.equal('You are not permitted to update your id');
-            done();
-          });
-      });
-
-      it('should return not found for invalid user id', (done) => {
-        const data = { username: 'smart', lastName: 'Johnson' };
-        superRequest.put('/users/99999')
-          .send(data)
-          .set({ 'x-access-token': adminToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(404);
-            expect(res.body.message).to.equal('This user does not exist');
-            done();
-          });
-      });
-
-      it(`should return permission denied when regular user want to
-        update another user's profile`, (done) => {
-        const data = { username: 'chinedu', lastname: 'ofor' };
-        superRequest.put(`/users/${newAdminUser.id}`)
-          .send(data)
-          .set({ 'x-access-token': regularToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(401);
-            expect(res.body.message).to
-              .equal('You are not permitted to update this profile');
-            done();
-          });
-      });
-
-      it('should give admin permission to update any user\'s profile',
-      (done) => {
-        const data = { username: 'chinedu', lastname: 'ofor' };
-        superRequest.put(`/users/${regularUser.id}`)
-          .send(data)
-          .set({ 'x-access-token': adminToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.message).to
-              .equal('Your profile has been updated');
-            expect(res.body.updatedUser.username).to.equal('chinedu');
-            expect(res.body.updatedUser.lastName).to.equal('ofor');
-            done();
-          });
-      });
-    });
-
-    describe('Logout', () => {
-      it('should logout successfully', (done) => {
-        superRequest.post('/users/logout')
-        .set({ 'x-access-token': adminToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            expect(res.body.message).to
-              .equal('You have successfully logged out');
-            done();
-          });
-      });
-      it('should not allow user to get user after logout', (done) => {
-        superRequest.get('/users')
-        .set({ 'x-access-token': adminToken })
-          .end((err, res) => {
-            expect(res.status).to.equal(401);
-            expect(res.body.message).to
-              .equal('Please sign in to access your account');
             done();
           });
       });
